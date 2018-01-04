37893ba70324be849ecdb02a2405da0ca1c74d40
liangcaifeng <551886714@qq.com>
2018-01-04 16:51:31 +0800

    first project

---
+++ vc-unitil/vc-unitil.h
@@ -0,0 +1,27 @@
+// vc-unitil.h : vc-unitil DLL 的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CvcunitilApp
+// 有关此类实现的信息，请参阅 vc-unitil.cpp
+//
+
+class CvcunitilApp : public CWinApp
+{
+public:
+	CvcunitilApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+	DECLARE_MESSAGE_MAP()
+};
